4a9e79f5b0848bf262ee53f9fb309564246a3583
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2014-07-29 10:21:04 +0200

    Write patch number into /opt/bb/.version

--- bigboards-updater/src/patch.sh
+++ bigboards-updater/src/patch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+[ -z $1 ] && echo -e "No patch identifier has been given. Please provide one as 1st parameter!" && exit 1
 PATCH=$1
 
 ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i /opt/bb/runtimes/bigboards-updater/hosts /opt/bb/runtimes/bigboards-updater/${PATCH}/install.yml && echo $1 > /opt/bb/.version
--- bigboards-updater/src/update.sh
+++ bigboards-updater/src/update.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i /opt/bb/runtimes/bigboards-updater/hosts $@ /opt/bb/runtimes/bigboards-updater/update.yml
+echo 'We are working hard to have update.sh apply the required consecutive patches...'
+echo 'In the mean time, please manually patch your Hex!'
+exit 1
+
