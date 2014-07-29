66f4e5a8c7f5f753f6c910ec1693158ca91585fb
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2014-07-29 09:52:19 +0200

    Write patch number into /opt/bb/.version

--- bigboards-updater/src/patch.sh
+++ bigboards-updater/src/patch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PATCH=$1
 
-ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i /opt/bb/runtimes/bigboards-updater/hosts /opt/bb/runtimes/bigboards-updater/${PATCH}/install.yml
+ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i /opt/bb/runtimes/bigboards-updater/hosts /opt/bb/runtimes/bigboards-updater/${PATCH}/install.yml && echo $1 > /opt/bb/.version
