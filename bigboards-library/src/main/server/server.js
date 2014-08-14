981b6cd614d79344f4b036716806ce723ab44109
Sven Meys <sven.meys@gmail.com>
2014-08-15 00:39:50 +0200

    Initial creation of the Community Library UI and ES datastore

--- bigboards-library/src/main/server/server.js
+++ bigboards-library/src/main/server/server.js
@@ -5,7 +5,8 @@ var express = require('express'),
     http = require('http'),
     path = require('path'),
     serverConfig = require('./config'),
-    winston = require('winston');
+    winston = require('winston'),
+    elasticSearch = require('elasticsearch');
 
 var app = module.exports = express();
 var server = http.createServer(app);
@@ -29,9 +30,44 @@ if (serverConfig.isDevelopment()) {
     app.use(express.errorHandler());
 }
 
+// Start elasticSearch
+var es = elasticSearch.Client({host : serverConfig.host + ':' + serverConfig.es_port });
+
+app.get("/library.json", function (req, res) {
+    es.search({
+        index: 'bigboards',
+        type: 'tints',
+
+        // Paging
+        from: req.query.from,
+        size: req.query.size || 100,
+
+        // Sorting: "field1:asc,field2:desc,..."
+        sort: req.query.sort,
+
+        // Basic querying
+        q: req.query.q || "*:*"
+
+    }, function(err, response, status) {
+        if (err) {
+            winston.error(err.message);
+            res.json(500, ["We are currently doing some maintenance"]);
+            return;
+        }
+
+        if (status == 200) {
+            res.json(status, response.hits.hits.map(function(document){return document._source;}));
+            return;
+        }
+
+        res.json(status, [])
+    });
+});
+
 /**********************************************************************************************************************
  * Start Server
  *********************************************************************************************************************/
 server.listen(app.get('port'), function () {
     console.log('bigboards library listening on port ' + app.get('port'));
+
 });
