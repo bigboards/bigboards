df2c046c07cb1c6a5aad699a9c55f3c0ee357996
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2014-08-26 23:40:55 +0200

    List all patches from the updater

--- bigboards-mmc/src/main/server/mods/firmware/index.js
+++ bigboards-mmc/src/main/server/mods/firmware/index.js
@@ -1,4 +1,8 @@
-var Firmware = function(tasks) {
+var Q = require('q');
+var fs = require("fs");
+
+var Firmware = function(patchesDirectory, tasks) {
+    this.patchesDirectory = patchesDirectory;
     this.tasks = tasks;
 };
 
@@ -9,6 +13,28 @@ var Firmware = function(tasks) {
  */
 Firmware.prototype.update = function() {
     return this.tasks.invoke('update');
+};
+
+/**
+ * Lists the known firmware patches.
+ *
+ * @returns [patch.name]
+ */
+Firmware.prototype.patches = function() {
+    var deferred = Q.defer();
+
+    var dir = this.patchesDirectory;
+    fs.readdir(dir, function(err, data) {
+        if (err) return deferred.reject(err);
+
+        try {
+            return deferred.resolve(data);
+        } catch (ex) {
+            return deferred.reject(ex)
+        }
+    });
+
+    return deferred.promise;
 };
 
 module.exports = Firmware;
