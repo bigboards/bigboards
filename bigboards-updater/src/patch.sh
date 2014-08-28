c191d125ac0fd8946b6ec6df8420a5704b9812a3
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2014-08-28 15:56:38 +0200

    .versions as patch file

--- bigboards-updater/src/patch.sh
+++ bigboards-updater/src/patch.sh
@@ -2,4 +2,4 @@
 [ -z $1 ] && echo -e "No patch identifier has been given. Please provide one as 1st parameter!" && exit 1
 PATCH=$1
 
-ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i /opt/bb/hosts /opt/bb/runtimes/bigboards-updater/${PATCH}/install.yml && echo $1 "|" $(date) >> /opt/bb/.version
+ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i /opt/bb/hosts /opt/bb/runtimes/bigboards-updater/${PATCH}/install.yml && echo $1 "|" $(date) >> /opt/bb/.versions
