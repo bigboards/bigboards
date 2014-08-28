9e2532f7c3a7dc5b0aa0d8a0bc39f53e02e1a164
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2014-08-28 10:06:14 +0200

    trigger install action on patches

--- bigboards-mmc/src/main/client/app/update/update.js
+++ bigboards-mmc/src/main/client/app/update/update.js
@@ -1,3 +1,9 @@
-updateModule.controller('UpdateController', function($scope, Patches) {
+updateModule.controller('UpdateController', function($scope, Patches, ApiFeedback) {
     $scope.patches = Patches.query();
+
+    $scope.installPatch = function(name) {
+        Patches.install({patch: name},
+        ApiFeedback.onSuccess("Succesfully submit patch '" + name + "' for installation!"),
+        ApiFeedback.onError());
+    }
 });
