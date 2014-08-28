dcbb5aa076b742b1ded8f522448cec7aca3b6a55
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2014-08-28 10:35:13 +0200

    install patch on success = navigate to install_patch task's output

--- bigboards-mmc/src/main/client/app/update/update.js
+++ bigboards-mmc/src/main/client/app/update/update.js
@@ -1,9 +1,13 @@
-updateModule.controller('UpdateController', function($scope, Patches, ApiFeedback) {
+updateModule.controller('UpdateController', function($scope, $location, Patches, ApiFeedback) {
     $scope.patches = Patches.query();
 
     $scope.installPatch = function(name) {
         Patches.install({patch: name},
-        ApiFeedback.onSuccess("Succesfully submit patch '" + name + "' for installation!"),
-        ApiFeedback.onError());
+            function() {
+                // navigate to tasks/install_patch/output
+                $location = "/#/tasks/install_patch/output";
+            },
+            ApiFeedback.onError()
+        );
     }
 });
