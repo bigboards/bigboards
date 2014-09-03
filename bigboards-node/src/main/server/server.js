1316c4a10eff3612906f6e385962d01cc8ee31fb
Daan Gerits <daan.gerits@bigboards.io>
2014-09-04 00:10:07 +0200

    we should really load the config before we use it.

--- bigboards-node/src/main/server/server.js
+++ bigboards-node/src/main/server/server.js
@@ -31,46 +31,49 @@ app.use(app.router);
  * Services
  *********************************************************************************************************************/
 var configuration = new Configuration(serverConfig.hex.file);
+configuration.load().then(function(config) {
+    var nodeService = new NodeService(
+        configuration.name,
+        configuration.node.sequence,
+        serverConfig.net.internal.itf,
+        serverConfig.net.external.itf
+    );
 
-var nodeService = new NodeService(
-    configuration.name,
-    configuration.sequence,
-    serverConfig.net.internal.itf,
-    serverConfig.net.external.itf
-);
+    var postman = new Postman(nodeService, os.hostname(), serfer, serverConfig.delay, [
+        {metric: 'load', fn: function(nodeService) { return nodeService.load(); }},
+        {metric: 'memory', fn: function(nodeService) { return nodeService.memory(); }},
+        {metric: 'temperature', fn: function(nodeService) { return nodeService.temperature(); }},
+        {metric: 'osDisk', fn: function(nodeService) { return nodeService.osDisk(); }},
+        {metric: 'dataDisk', fn: function(nodeService) { return nodeService.dataDisk(); }},
+        {metric: 'network', fn: function(nodeService) {
+            return {
+                internalIp: nodeService.internalIpAddress(),
+                externalIp: nodeService.externalIpAddress()
+            };
+        }},
+        {metric: 'container', fn: function(nodeService) { return nodeService.container(); }}
+    ]);
 
-var postman = new Postman(nodeService, os.hostname(), serfer, serverConfig.delay, [
-    {metric: 'load', fn: function(nodeService) { return nodeService.load(); }},
-    {metric: 'memory', fn: function(nodeService) { return nodeService.memory(); }},
-    {metric: 'temperature', fn: function(nodeService) { return nodeService.temperature(); }},
-    {metric: 'osDisk', fn: function(nodeService) { return nodeService.osDisk(); }},
-    {metric: 'dataDisk', fn: function(nodeService) { return nodeService.dataDisk(); }},
-    {metric: 'network', fn: function(nodeService) {
-        return {
-            internalIp: nodeService.internalIpAddress(),
-            externalIp: nodeService.externalIpAddress()
-        };
-    }},
-    {metric: 'container', fn: function(nodeService) { return nodeService.container(); }}
-]);
+    /**********************************************************************************************************************
+     * Routes
+     *********************************************************************************************************************/
+    var routes = new Routes(nodeService);
+    routes.link(app);
 
-/**********************************************************************************************************************
- * Routes
- *********************************************************************************************************************/
-var routes = new Routes(nodeService);
-routes.link(app);
+    /**********************************************************************************************************************
+     * Start Server
+     *********************************************************************************************************************/
+    serfer.connect().then(function() {
+        postman.startDelivery();
+        var self = this;
 
-/**********************************************************************************************************************
- * Start Server
- *********************************************************************************************************************/
-serfer.connect().then(function() {
-    postman.startDelivery();
-    var self = this;
-
-    server.listen(app.get('port'), function () {
-        winston.info('BigBoards-node listening on port ' + app.get('port'));
+        server.listen(app.get('port'), function () {
+            winston.info('BigBoards-node listening on port ' + app.get('port'));
+        });
     });
 });
+
+
 
 /**********************************************************************************************************************
  * Handle all exceptions instead of bailing
