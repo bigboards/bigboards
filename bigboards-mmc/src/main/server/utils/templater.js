2ef6249685bef55ef37bd919770977519c2697da
Daan Gerits <daan.gerits@bigboards.io>
2014-09-07 21:28:31 +0200

    fixed the templating mechanism

--- bigboards-mmc/src/main/server/utils/templater.js
+++ bigboards-mmc/src/main/server/utils/templater.js
@@ -5,6 +5,30 @@ function Templater(configuration) {
     this.configuration = configuration;
 }
 
+Templater.prototype.templateString = function(string, nodes) {
+    var scope = {};
+
+    scope.hex = {
+        id: this.configuration.id,
+        name: this.configuration.name
+    };
+
+    scope.nodes = {};
+    nodes.forEach(function (node) {
+        scope.nodes[node.name] = {
+            ip: node.network.externalIp,
+            status: node.status
+        };
+
+        scope.nodes[node.container.name] = {
+            ip: node.container.externalIp,
+            status: node.container.status
+        };
+    });
+
+    return swig.render(string, { locals: scope });
+};
+
 Templater.prototype.template = function(file, nodes) {
     var scope = {};
 
@@ -26,7 +50,7 @@ Templater.prototype.template = function(file, nodes) {
         };
     });
 
-    return swig.renderFile(file, scope);
+    return swig.renderFile(file, { locals: scope });
 };
 
 module.exports = Templater;
