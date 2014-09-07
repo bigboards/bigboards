fa24b61285c067971af343052a532a84096567c6
Daan Gerits <daan.gerits@bigboards.io>
2014-09-07 20:19:41 +0200

    the templater doesn't what it is supposed to do.

--- bigboards-mmc/src/main/server/utils/templater.js
+++ bigboards-mmc/src/main/server/utils/templater.js
@@ -1,41 +1,32 @@
 var swig = require('swig'),
     Q = require('q');
 
-function Templater(configuration, services) {
+function Templater(configuration) {
     this.configuration = configuration;
-    this.services = services;
 }
 
 Templater.prototype.template = function(file, nodes) {
-    var deferrer = Q.defer();
+    var scope = {};
 
-    try {
-        var scope = {};
+    scope.hex = {
+        id: this.configuration.id,
+        name: this.configuration.name
+    };
 
-        scope.hex = {
-            id: this.configuration.id,
-            name: this.configuration.name
+    scope.nodes = {};
+    nodes.forEach(function (node) {
+        scope.nodes[node.name] = {
+            ip: node.network.externalIp,
+            status: node.status
         };
 
-        scope.nodes = {};
-        nodes.forEach(function (node) {
-            scope.nodes[node.name] = {
-                ip: node.network.externalIp,
-                status: node.status
-            };
+        scope.nodes[node.container.name] = {
+            ip: node.container.externalIp,
+            status: node.container.status
+        };
+    });
 
-            scope.nodes[node.container.name] = {
-                ip: node.container.externalIp,
-                status: node.container.status
-            };
-        });
-
-        deferrer.resolve(swig.renderFile(file, scope));
-    } catch (error) {
-        deferrer.reject(error);
-    }
-
-    return deferrer.promise;
+    return swig.renderFile(file, scope);
 };
 
 module.exports = Templater;
