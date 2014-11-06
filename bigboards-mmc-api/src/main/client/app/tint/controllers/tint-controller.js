d85fcf7b94fdb91f321fc626205e02743e1cb44c
Daan Gerits <daan.gerits@bigboards.io>
2014-11-06 22:25:15 +0100

    added: tint.delete, tint.update, node metrics removed: milestone & release

--- bigboards-mmc-api/src/main/client/app/tint/controllers/tint-controller.js
+++ bigboards-mmc-api/src/main/client/app/tint/controllers/tint-controller.js
@@ -1,6 +1,25 @@
-app.controller('TintCtrl', ['$scope', '$stateParams', 'Tints',
-                function(    $scope,   $stateParams,   Tints) {
+app.controller('TintCtrl', ['$scope', '$stateParams', 'Tints', 'ApiFeedback',
+                function(    $scope,   $stateParams,   Tints,   ApiFeedback) {
 
     $scope.tint = Tints.get({type: $stateParams.type, tintId: $stateParams.tintId});
 
+    $scope.update = function() {
+        Tints.update(
+            {type: $scope.tint.type, id: encodeURIComponent($scope.tint.id)},
+            ApiFeedback.onSuccess("Successfully updated the " + $scope.tint.name + " tint"),
+            ApiFeedback.onError()
+        );
+    };
+
+    $scope.removeTint = function() {
+        var confirmed = confirm("Are you sure? This will remove the " + $scope.tint.name + " tint from the hex.");
+
+        if (confirmed) {
+            Tints.remove(
+                {type: $scope.tint.type, id: encodeURIComponent($scope.tint.id)},
+                ApiFeedback.onSuccess("Successfully removed the " + $scope.tint.name + " tint from the hex"),
+                ApiFeedback.onError()
+            );
+        }
+    };
 }]);
