a9d7438e7a8b92c93125f1d0da4058d508a336d0
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2014-11-12 16:43:20 +0100

    Integrate ApiDoc.js in our builds

--- bigboards-mmc-api/GruntFile.js
+++ bigboards-mmc-api/GruntFile.js
@@ -8,14 +8,19 @@ module.exports = function(grunt) {
 
     gtx.loadNpmTasks('grunt-debug-task');
 
-    gtx.loadNpmTasks("grunt-bower-install-simple");
-    gtx.registerTask("bower-install", [ "bower-install-simple" ]);
+    // grunt bower integration
+    gtx.loadNpmTasks('grunt-bower-install-simple');
+    gtx.registerTask('bower-install', [ 'bower-install-simple' ]);
+
+    // ApiDoc.js
+    gtx.loadNpmTasks('grunt-apidoc');
+    gtx.registerTask('apidoc', ['apidoc']);
 
     gtx.config(gruntConfig);
 
     // We need our bower components in order to develop
     gtx.alias('build:dev',  ['recess:app', 'copy:dev']);
-    gtx.alias('build:dist', ['bower-install:dev', 'clean:dist', 'copy:dist', 'clean:dists', 'recess:min', 'concat:dist', 'uglify:dist']);
+    gtx.alias('build:dist', ['bower-install:dev', 'clean:dist', 'copy:dist', 'clean:dists', 'recess:min', 'concat:dist', 'uglify:dist', 'apidoc:mmc-api']);
 
     gtx.alias('release', ['bower-install:dev', 'build:dev', 'changelog:release', 'bump-commit']);
     gtx.alias('release-patch', ['bump-only:patch', 'release']);
