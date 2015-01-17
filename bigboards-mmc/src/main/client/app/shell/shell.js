fbc9ef12d460e678337621ae781cf427b01d4d83
Daan Gerits <daan.gerits@bigboards.io>
2015-01-17 21:18:09 +0100

    added the shell to the user interface

--- bigboards-mmc/src/main/client/app/shell/shell.js
+++
@@ -1,3 +0,0 @@
-shellModule.controller('ShellController', function($rootScope, $scope, $sce, $location) {
-    $scope.shellUrl = $sce.trustAsResourceUrl($location.protocol() + '://' + $location.host() + ':57575/');
-});
