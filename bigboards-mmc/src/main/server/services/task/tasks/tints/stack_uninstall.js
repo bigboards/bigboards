e0094d452d0f2c6be8782e2adbf294f86ab05c66
Daan Gerits <daan.gerits@bigboards.io>
2015-01-26 23:04:15 +0100

    changed the uninstall script

--- bigboards-mmc/src/main/server/services/task/tasks/tints/stack_uninstall.js
+++ bigboards-mmc/src/main/server/services/task/tasks/tints/stack_uninstall.js
@@ -23,31 +23,33 @@ module.exports = function(configuration, services) {
         execute: function(scope) {
              // -- TODO: check if the uninstall script exists
 
-            return services.hex
-                .get()
+            return services.hex.get()
                 .then(function(hex) {
                     scope.hex = hex;
+                })
+                .then(function() {
+                    return services.library.getTint(scope.tint.type, scope.tint.owner, scope.tint.id);
+                })
+                .then(function(ft) {
+                    console.log("Update the tint state to 'partial'");
+                    scope.tintMeta = ft;
+                    scope.tintMeta['state'] = 'partial';
+                    scope.tintMetaString = JSON.stringify(scope.tintMeta);
 
-                    return services.library
-                        .getTint(scope.tint.type, scope.tint.owner, scope.tint.id)
-                        .then(function(ft) {
-                            console.log("Update the tint state to 'partial'");
-                            scope.tintMeta = ft;
-                            scope.tintMeta['state'] = 'partial';
-                            scope.tintMetaString = JSON.stringify(scope.tintMeta);
-
-                            return scope;
-                        })
-                        .then(TaskUtils.playbook({
+                    return scope;
+                })
+                .then(function() {
+                    TaskUtils.playbook({
                             playbook: '_uninstall',
                             scope: scope,
                             hosts: '_hosts',
                             path: '/opt/bb/tints.d/' + scope.tint.type + '/' + scope.tint.owner + '/' + scope.tint.id + '/work'
-                        }))
-                        .then(TaskUtils.removeFile(
-                            '/opt/bb/tints.d/' + scope.tint.type + '/' + scope.tint.owner + '/' + scope.tint.id
-                        ));
-            });
+                        })
+                })
+                .then(function() {
+                    TaskUtils.removeFile('/opt/bb/tints.d/' + scope.tint.type + '/' + scope.tint.owner + '/' + scope.tint.id);
+                });
+
         }
     };
 };
