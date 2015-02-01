95a6dc18f39247956886357dfd1f1ea9e3d8c8cf
Daan Gerits <daan.gerits@bigboards.io>
2015-02-01 17:05:23 +0100

    added docker instances and changed the bb command a bit

--- bigboards-docker/build_all.sh
+++ bigboards-docker/build_all.sh
@@ -6,7 +6,7 @@ DOCKER_USER=bigboards
 DOCKER_PASSWORD=9jUlOzHTol14
 DOCKER_EMAIL=hello@bigboards.io
 
-REPOSITORIES="base java-7 nodejs hadoop"
+REPOSITORIES="base java-7 nodejs hadoop elasticsearch elasticsearch-kibana elasticsearch-logstash elasticsearch-marvel elasticsearch-shield skylable zookeeper storm"
 DOCKER="/usr/bin/docker"
 
 echo "Logging into the docker hub"
@@ -14,6 +14,6 @@ ${DOCKER} login -e ${DOCKER_EMAIL} -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 
 for REPO in ${REPOSITORIES}
 do
-    ./build ${REPO}
+    ./build.sh ${REPO}
 done
 
