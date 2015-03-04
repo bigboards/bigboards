064d247b8b516e99a93e0d9f509d2dcb0880b341
Daan Gerits <daan.gerits@bigboards.io>
2015-03-04 12:14:26 +0100

    updated the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-deb-s3 upload -p -b apt.bigboards.io -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/dist/*.deb
-deb-s3 upload -p -b apt.bigboards.io -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/dist/*.deb
-deb-s3 upload -p -b apt.bigboards.io -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/dist/*.deb
+deb-s3 upload -p -b apt.bigboards.io -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
