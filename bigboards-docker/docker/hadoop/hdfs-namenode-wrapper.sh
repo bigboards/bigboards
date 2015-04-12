89d7836684c953258e07e75bd7956b2da5bd747b
Daan Gerits <daan.gerits@bigboards.io>
2015-04-12 18:40:59 +0200

    added a build operation to the container bb command

--- bigboards-docker/docker/hadoop/hdfs-namenode-wrapper.sh
+++ bigboards-docker/docker/hadoop/hdfs-namenode-wrapper.sh
@@ -5,7 +5,7 @@ DAEMON=$1
 if [ -e "$HDFS_FORMAT_MARKER" ]; then
     echo "Found the HDFS format marker. Formatting hdfs before starting";
 
-    /opt/hadoop/bin/hadoop --config /opt/hadoop/etc/hadoop namenode -format -nonInteractive
+    /opt/hadoop/bin/hadoop --config /opt/hadoop/etc/hadoop namenode -format -force -nonInteractive
 
     if [ $? -eq 0 ]; then
         rm -rf "$HDFS_FORMAT_MARKER"
