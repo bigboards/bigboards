fd111f662a1c74e842988ae430fb4eba58b9b679
Daan Gerits <daan.gerits@gmail.com>
2015-04-22 13:25:25 +0000

    deploy.sh edited online with Bitbucket

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch i386 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch all -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
 
-deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch i386 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch all -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
 
-deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch i386 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch all -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
