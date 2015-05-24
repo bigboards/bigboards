a9eb8a3005d1d6520f659ef24ac69cbbcec4103a
Daan Gerits <daan@bigboards.io>
2015-05-24 10:43:05 +0200

    had to modify the deploy script so the correct architectures are being created. It gave errors during deployment to S3

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-deb-s3 upload -p -b apt.bigboards.io --arch all -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
 
-deb-s3 upload -p -b apt.bigboards.io --arch all -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
 
-deb-s3 upload -p -b apt.bigboards.io --arch all -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
