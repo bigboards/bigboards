a1c9bf3fc96f5855fc5d0e4e23910349aa0e72ca
Daan Gerits <daan@bigboards.io>
2015-05-24 10:44:41 +0200

    dont know if armhf or armv7l is the right arch

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
 
 deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
 
 deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
