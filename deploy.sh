0698ad8166ef390eeacff6a9eba26370de70d436
Daan Gerits <daan@bigboards.io>
2015-07-27 10:16:33 +0200

    typo

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 IFS='/' read -a array <<< "$1"
-if [ "${array[@]}" -eq "2" ]; then
 BRANCH="${array[2]}"
-else
-BRANCH="$1"
-fi
 
 deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
