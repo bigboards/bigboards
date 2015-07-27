27817ac497aeb56f1bd9a483d0420f09262682ad
Daan Gerits <daan@bigboards.io>
2015-07-27 10:25:18 +0200

    changed the name of the architecture from x86_64 to amd64

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ BRANCH="${array[2]}"
 
 deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch amd64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
 
 deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch amd64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
 
 deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch amd64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
