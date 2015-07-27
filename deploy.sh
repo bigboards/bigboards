b77d22712b29968e2276b1e52720616d51cf1035
Daan Gerits <daan@bigboards.io>
2015-07-27 10:10:41 +0200

    allowed the branch to be read in git format

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+IFS='/' read -a array <<< "$1"
+if [ "${array[@]}" -eq "2" ]; then
+BRANCH="${array[2]}"
+else
+BRANCH="$1"
+fi
 
-deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
 
-deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
-deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $1 --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
+
+deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch x86_64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-updater/target/*.deb
