1bfe9f319c18319281a8ab4c49a6a72101acd77e
Daan Gerits <daan@bigboards.io>
2015-08-24 08:29:00 +0200

    Revert "removed the bigboards-cli project from the general project"
    This reverts commit c8b436231ca20cbd52d8d082170f02b4cb6df73e.
    
    Conflicts:
    	bigboards-cli/src/cli/ansible/system/bootstrap/bootstrap.yml

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,10 @@
 IFS='/' read -a array <<< "$1"
 BRANCH="${array[2]}"
 
+deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+deb-s3 upload -p -b apt.bigboards.io --arch amd64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-cli/target/*.deb
+
 deb-s3 upload -p -b apt.bigboards.io --arch armv7l -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io --arch armhf -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io --arch amd64 -c $BRANCH --access-key-id=$2 --secret-access-key=$3 bigboards-mmc/target/*.deb
