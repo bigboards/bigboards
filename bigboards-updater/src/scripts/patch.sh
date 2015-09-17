8826735725897247719f3946c683f5c25d294dba
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2015-09-17 11:58:56 +0200

    Implement hostsfile detection as done in BB command

--- bigboards-updater/src/scripts/patch.sh
+++ bigboards-updater/src/scripts/patch.sh
@@ -2,4 +2,7 @@
 [ -z $1 ] && echo -e "No patch identifier has been given. Please provide one as 1st parameter!" && exit 1
 PATCH=$1
 
-ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i /opt/bb/hosts /opt/bb/runtimes/bigboards-updater/patches/${PATCH}/install.yml && echo $1 "|" $(date) >> /opt/bb/.versions
+HOSTSFILE=/opt/bb/hosts
+[ ! -f "$HOSTSFILE" ] && HOSTSFILE=/etc/ansible/hosts
+
+ANSIBLE_ERROR_ON_UNDEFINED_VARS=True ansible-playbook -i ${HOSTSFILE} /opt/bb/runtimes/bigboards-updater/patches/${PATCH}/install.yml && echo $1 "|" $(date) >> /opt/bb/.versions
