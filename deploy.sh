6536521477f75bae72da0c56eea4b4dc694c1cd6
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2016-02-17 23:00:34 +0100

    export AWS_PROFILE

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BRANCH="$BUILDKITE_BRANCH"
-AWS_PROFILE=personal
+export AWS_PROFILE=personal
 
 deb-s3 upload -p -b apt.bigboards.io -a armv7l -c $BRANCH bigboards-cli/target/*.deb
 deb-s3 upload -p -b apt.bigboards.io -a armhf -c $BRANCH bigboards-cli/target/*.deb
