ab73a062ceaa19db39f0f6612b4e01efc9c4273a
Daan Gerits <daan.gerits@gmail.com>
2017-02-01 12:19:51 +0100

    changed the tint system

--- bigboards-updater/src/ansible/20150503/files/etc/ganglia-webfrontend/conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-# Where gmetad stores the rrd archives.
-$conf['gmetad_root'] = "/data/ganglia";
-$conf['rrds'] = "${conf['gmetad_root']}/rrds";
-
-?>
---
+++ bigboards-updater/src/ansible/files/etc/ganglia-webfrontend/conf.php
@@ -0,0 +1,7 @@
+<?php
+
+# Where gmetad stores the rrd archives.
+$conf['gmetad_root'] = "/data/ganglia";
+$conf['rrds'] = "${conf['gmetad_root']}/rrds";
+
+?>
