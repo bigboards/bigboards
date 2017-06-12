2b75b614c87a0bc6c10572c308a9774fdd349117
Wim Van Leuven <wim.vanleuven@bigboards.io>
2017-06-12 09:22:52 +0200

    fix bb-cluster-init scripts - use node names in ansible_hosts file instead of IPs - add trailing spaces on user prompts

--- bigboards-cli/src/cli/scripts/bb-cluster-init.sh
+++ bigboards-cli/src/cli/scripts/bb-cluster-init.sh
@@ -4,38 +4,38 @@
 read -r -p "What is the name of this cluster? " CLUSTER_NAME
 
 # -- enter the ip address of the master node
-read -r -p "Enter the internal ip addresses of the node participating as the cluster master" MASTER_ADDRESS
+read -r -p "Enter the internal ip addresses of the node participating as the cluster master: " MASTER_ADDRESS
 re='^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$'
 while ! [[ $MASTER_ADDRESS =~ $re ]] ; do
     echo "invalid input!"
-    read -r -p "Enter the internal ip addresses of the nodes participating in the cluster. Separate multiple entries with spaces" MASTER_ADDRESS
+    read -r -p "Enter the internal ip addresses of the node participating as the cluster master: " MASTER_ADDRESS
 done
 
 # -- list the ip addresses of the nodes participating in the cluster
-read -r -p "Enter the internal ip addresses of the nodes participating in the cluster. Separate multiple entries with spaces" NODE_ADDRESSES
+read -r -p "Enter the internal ip addresses of the nodes participating in the cluster. Separate multiple entries with spaces: " NODE_ADDRESSES
 re='^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}( (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})*$'
 while ! [[ $NODE_ADDRESSES =~ $re ]] ; do
     echo "invalid input!"
-    read -r -p "Enter the internal ip addresses of the nodes participating in the cluster. Separate multiple entries with spaces" NODE_ADDRESSES
+    read -r -p "Enter the internal ip addresses of the nodes participating in the cluster. Separate multiple entries with spaces: " NODE_ADDRESSES
 done
 
 NODE_COUNT=$(wc -w <<< "$NODE_ADDRESSES")
-echo "$NODE_COUNT worker node addresses entered"
+echo "$NODE_COUNT worker node addresses entered."
 
 # -- enter the netmask on which the nodes are communicating
-read -r -p "Enter the cluster netmask" NETMASK
+read -r -p "Enter the cluster's netmask: " NETMASK
 re='^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$'
 while ! [[ $NETMASK =~ $re ]] ; do
     echo "invalid input!"
-    read -r -p "Enter the cluster netmask" NETMASK
+    read -r -p "Enter the cluster's netmask: " NETMASK
 done
 
 cat > /tmp/ansible_hosts << EOF
 [all:vars]
-bb_netmask: $NETMASK
+bb_netmask: ${NETMASK}
 bb_nic_int: eth0
 bb_nic_ext: eth1
-bb_dns: $MASTER_ADDRESS
+bb_dns: ${MASTER_ADDRESS}
 
 [local]
 localhost   connection=local
@@ -45,14 +45,14 @@ host-coordinators
 host-workers
 
 [host-coordinators]
-$MASTER_ADDRESS   ansible_ssh_user=bb   bb_node_sequence=1  bb_node_address=$MASTER_ADDRESS
+${CLUSTER_NAME}-n1   ansible_ssh_user=bb   bb_node_sequence=1  bb_node_address=${MASTER_ADDRESS}
 
 [host-workers]
 EOF
 
 SEQ=2
 for i in $NODE_ADDRESSES; do
-    echo "${i}     ansible_ssh_user=bb     bb_node_sequence=${SEQ}  bb_node_address=$i" >> /tmp/ansible_hosts
+    echo "${CLUSTER_NAME}-n${SEQ}   ansible_ssh_user=bb     bb_node_sequence=${SEQ}  bb_node_address=$i" >> /tmp/ansible_hosts
     let SEQ=SEQ+1
 done
 
