0bee71c86654aa1d5d5477fdcd659b9cfad83b37
Wim Van Leuven <wim.vanleuven@bigboards.io>
2017-10-24 17:16:05 +0200

    revert to old version of bb-cluster-init.sh script

--- bigboards-cli/src/cli/scripts/bb-cluster-init.sh
+++ bigboards-cli/src/cli/scripts/bb-cluster-init.sh
@@ -3,40 +3,19 @@
 # -- get the cluster name
 read -r -p "What is the name of this cluster? " CLUSTER_NAME
 
-# -- enter the ip address of the master node
-read -r -p "Enter the internal ip addresses of the node participating as the cluster master: " MASTER_ADDRESS
-re='^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$'
-while ! [[ $MASTER_ADDRESS =~ $re ]] ; do
-    echo "invalid input!"
-    read -r -p "Enter the internal ip addresses of the node participating as the cluster master: " MASTER_ADDRESS
+# -- get the number of nodes
+read -r -p "How many nodes are in this cluster? " NODE_COUNT
+re='^[0-9]+$'
+while ! [[ $NODE_COUNT =~ $re ]] ; do
+   read -r -p "How many nodes are in this cluster? " NODE_COUNT
 done
 
-# -- list the ip addresses of the nodes participating in the cluster
-read -r -p "Enter the internal ip addresses of the nodes participating in the cluster. Separate multiple entries with spaces: " NODE_ADDRESSES
-re='^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}( (25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3})*$'
-while ! [[ $NODE_ADDRESSES =~ $re ]] ; do
-    echo "invalid input!"
-    read -r -p "Enter the internal ip addresses of the nodes participating in the cluster. Separate multiple entries with spaces: " NODE_ADDRESSES
-done
-
-NODE_COUNT=$(wc -w <<< "$NODE_ADDRESSES")
-echo "$NODE_COUNT worker node addresses entered."
-
-# -- enter the netmask on which the nodes are communicating
-read -r -p "Enter the cluster's netmask: " NETMASK
-re='^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$'
-while ! [[ $NETMASK =~ $re ]] ; do
-    echo "invalid input!"
-    read -r -p "Enter the cluster's netmask: " NETMASK
-done
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
 
 cat > /tmp/ansible_hosts << EOF
-[all:vars]
-bb_netmask: ${NETMASK}
-bb_nic_int: eth0
-bb_nic_ext: eth1
-bb_dns: ${MASTER_ADDRESS}
-
 [local]
 localhost   connection=local
 
@@ -45,36 +24,15 @@ host-coordinators
 host-workers
 
 [host-coordinators]
-${CLUSTER_NAME}-n1   ansible_ssh_user=bb   bb_node_sequence=1  bb_node_address=${MASTER_ADDRESS}
+${CLUSTER_NAME}-n1   ansible_ssh_user=bb
 
 [host-workers]
 EOF
 
-SEQ=2
-for i in $NODE_ADDRESSES; do
-    echo "${CLUSTER_NAME}-n${SEQ}   ansible_ssh_user=bb     bb_node_sequence=${SEQ}  bb_node_address=$i" >> /tmp/ansible_hosts
-    let SEQ=SEQ+1
+for i in $(seq 2 ${NODE_COUNT}); do
+    echo "${CLUSTER_NAME}-n${i}     ansible_ssh_user=bb" >> /tmp/ansible_hosts
 done
 
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -y ansible
-
-#sudo mv /tmp/ansible_hosts /etc/ansible/hosts
-
-# -- set variables depending on the architecture
-#ARCHITECTURE=$(uname -m)
-#INT_NIC="eth0"
-#EXT_NIC="eth1"
-#REF_NIC="eth0"
-
-#HAS_BRIDGE=$([ $(ifconfig br1 2> /dev/null |wc -l) -gt 0 ] && echo 'yes' || echo 'no')
-#if [ "$ARCHITECTURE" = "armv7l" && "$HAS_BRIDGE" = "yes" ]; then
-#    REF_NIC="br0"
-#fi
-
-#IP_RANGE=$(echo `ifconfig $REF_NIC 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'` | rev | cut -c 3- | rev)
-#NETMASK=$(echo $(ifconfig $REF_NIC |grep Mask | tr -s ' ' | cut -d ':' -f4))
+sudo mv /tmp/ansible_hosts /etc/ansible/hosts
 
 exit $?
